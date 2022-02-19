499f573fe139db70f9a851c93d4fa4188a4d407d
Thuthuy1311 <72400802+Thuthuy1311@users.noreply.github.com>
2022-02-19 22:51:14 +0700

    update form, reactiveform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BoldDirective } from './Directives/bold.directive';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +19,15 @@ import { BoldDirective } from './Directives/bold.directive';
     HomeComponent,
     AboutComponent,
     PageNotFoundComponent,
-    BoldDirective
+    BoldDirective,
+    ReactiveFormComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
